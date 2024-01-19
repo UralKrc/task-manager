9da8067cf9b722f221d2e22f397b9dced713b47f
Ural <ur.karaca@gmail.com>
2024-01-19 01:00:36 +0100

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,30 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jest-environment-jsdom",
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: "node_modules/ts-jest-mock-import-meta",
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    VITE_SUPABASE_URL: "https://www.url.com",
+                    VITE_SUPABASE_ANON_KEY: "123",
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
-  setupFiles: ["<rootDir>/.jest/env.js"],
   moduleNameMapper: {
     "^.+\\.svg$": "jest-svg-transformer",
     "^.+\\.(css|less|scss)$": "identity-obj-proxy",
--- src/helpers/supabaseClient.ts
+++ src/helpers/supabaseClient.ts
@@ -1,10 +1,15 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Environment variables VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY are required');
+  throw new Error(
+    "Environment variables SUPABASE_URL and SUPABASE_ANON_KEY are required"
+  );
 }
 
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase: SupabaseClient = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
